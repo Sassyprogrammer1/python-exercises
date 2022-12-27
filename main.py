53f8d3f0fea21946c62db9e4ad9abc81a54e41bc
cuuj69 <mensahjefferson69@gmail.com>
2022-12-27 16:56:04 +0000

    modified changes

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from datetime import datetime
 
-
-# # datetime.today().strftime('%Y-%m-%d')
 current_date = datetime.now()
-# # print(current_date.isoformat())
 
 converted = current_date.strftime('%Y-%m-%d')
-
-# print(converted)
-# .strftime('%Y-%m-%dT%H:%M:%S.%f%z')
 
 def dateConversion(date):
     return current_date.isoformat()
 
 results = dateConversion(converted)
-
 print(results)

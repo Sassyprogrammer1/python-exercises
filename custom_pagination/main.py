13c5f8d3077ea9e7370a78e9672ebdf5541d2d23
cuuj69 <mensahjefferson69@gmail.com>
2022-12-28 17:57:43 +0000

    modified code

--- custom_pagination/main.py
+++ custom_pagination/main.py
@@ -6,14 +6,10 @@ app = FastAPI()
 
 with open('./data.json') as f:
     data = json.load(f)
-data_length = len(data)
+data_length = len(data)~
 
 
 
 @app.get("/posts")
 def read_posts():
     return data
-
-@app.get("https://jsonplaceholder.typicode.com/posts")
-def get_data():
-    return

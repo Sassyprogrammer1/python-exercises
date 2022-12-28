56a3ca6de151faf45c43cfecf08317d6c12f26c0
cuuj69 <mensahjefferson69@gmail.com>
2022-12-28 19:24:09 +0000

    modified code

--- custom_pagination/main.py
+++ custom_pagination/main.py
@@ -11,5 +11,7 @@ data_length = len(data)
 
 
 @app.get("/posts")
-def read_posts(page_num: int = 10, page_size: int = 10):
-    return data
+def read_posts(page_num: int = 1, page_size: int = 10):
+    start = (page_num - 1) * page_size
+    end = start + page_size
+    return data[start:end]

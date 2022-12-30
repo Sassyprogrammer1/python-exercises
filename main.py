d25626db7f07b2487eb4de5f5645f6f9b1c0ec05
cuuj69 <mensahjefferson69@gmail.com>
2022-12-30 11:14:50 +0000

    Implemented post end point to return datetime in ISO format

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+from fastapi import FastAPI
 from datetime import datetime
+
+app = FastAPI()
 
 current_date = datetime.now()
 
 converted = current_date.strftime('%Y-%m-%d')
-
-def dateConversion(date):
+@app.post("/post")
+def dateConversion(date:str=converted):
     return current_date.isoformat()
 
-results = dateConversion(converted)
-print(results)

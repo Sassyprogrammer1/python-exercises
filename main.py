c445f603f82e4464d2fe06228866a5d60f8ba7ad
cuuj69 <mensahjefferson69@gmail.com>
2023-01-01 23:20:26 +0000

    Defined endpoints for today,this_week, this_month

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ converted = current_date.strftime('%Y-%m-%d')
 def dateConversion(date:str=converted):
     return current_date.isoformat()
 
+@app.get("/today")
+def today():
+    # To return the date range for the past 24 hours
+    return 
+
+@app.get("/lastweek")
+def this_week():
+    # To return the date range for the past 7 days
+    return
+
+@app.get("/thismonth")
+def this_month():
+    # To return the date range for the current month from the beginnng of it,ie from 1st January to 2nd
+    return 
+
+

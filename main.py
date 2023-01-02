860467838a4613dc4c5ee48400e71a70d12722ce
cuuj69 <mensahjefferson69@gmail.com>
2023-01-02 01:10:15 +0000

    Implemented responses for different endpoints

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from datetime import datetime
+from datetime import datetime,timedelta
 
 app = FastAPI()
 
 current_date = datetime.now()
-
 converted = current_date.strftime('%Y-%m-%d')
+
 @app.post("/api")
 def dateConversion(date:str=converted):
     return current_date.isoformat()
@@ -13,16 +13,25 @@ def dateConversion(date:str=converted):
 @app.get("/today")
 def today():
     # To return the date range for the past 24 hours
-    return 
-
+    base = datetime.now()
+    hours = [base - timedelta(hours=x) for x in range(24)]
+    return hours
+  
 @app.get("/lastweek")
 def this_week():
-    # To return the date range for the past 7 days
-    return
+    base = datetime.today()
+    date_list = [base - timedelta(days=x) for x in range(7)]
+    return date_list
+
 
 @app.get("/thismonth")
 def this_month():
-    # To return the date range for the current month from the beginnng of it,ie from 1st January to 2nd
-    return 
+    base = datetime.today()
+    days = [base + timedelta(days=x-1) for x in range(31)]
+    return days
+
+
+
+
 
 

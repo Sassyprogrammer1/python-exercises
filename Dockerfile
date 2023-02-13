fceef461b0712e2dbab54b40675b4519f3ce386c
cuuj69 <mensahjefferson69@gmail.com>
2023-02-13 23:10:43 +0000

    Added:Dockerfile and modified some code

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.9-alpine
+
+WORKDIR /app
+
+COPY stringify /app
+
+RUN pip install --upgrade pip
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Set the default command to run when starting the container
+CMD ["python", "stringify.py"]
---
+++ stringify.py
@@ -0,0 +1,39 @@
+import json
+
+
+with open('F:\\PythonProjects\\python-exercises\\converted.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+
+def wrap_in_quotes(field, obj):
+    # If the object is a dictionary, loop through its key-value pairs
+    if isinstance(obj, dict):
+        # If the field name is in the dictionary, wrap its value in quotes
+        if field in obj:
+            obj[field] = f'"{obj[field]}"'
+        # Recursively call the `wrap_in_quotes` function for each value in the dictionary
+        return {k: wrap_in_quotes(field, v) for k, v in obj.items()}
+    # If the object is a list, recursively call the `wrap_in_quotes` function for each element in the list
+    elif isinstance(obj, list):
+        return [wrap_in_quotes(field, elem) for elem in obj]
+    # If the object is neither a dict nor a list, return the object as-is
+    else:
+        return obj
+
+
+# Wrap the values of the field 'tuition' in quotes
+data = wrap_in_quotes('tuition', data)
+
+# Wrap the values of the field 'applicationDeadline' in quotes
+data = wrap_in_quotes('applicationDeadline', data)
+
+# Wrap the values of the field 'requirements' in quotes
+data = wrap_in_quotes('requirements', data)
+
+
+
+data = wrap_in_quotes('langRequirement', data)
+
+
+with open('F:\\PythonProjects\\python-exercises\\converted.json', 'w') as file:
+    json.dump(data, file, indent=4)
